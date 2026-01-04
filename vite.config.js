ce2d82013128031ed527fc90f1ea0e4360f6ccf9
Krish-ai007 <krishpachchigar9@gmail.com>
2026-01-04 12:35:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DeliQu_foods/', // 👈 REQUIRED for GitHub Pages (repo name)
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/',
+  plugins: [react()],
 })
+
